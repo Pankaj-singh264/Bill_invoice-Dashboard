7c73659029dd0ffef7e5d2786457c1169e02feab
Pankaj <pn00532@gmail.com>
2025-05-05 18:13:34 +0530

    half render

--- billInvoiceFrontEnd/src/App.jsx
+++ billInvoiceFrontEnd/src/App.jsx
@@ -18,11 +18,14 @@ export default function App() {
         <AuthProvider>
           <Routes>
             {/* Public routes */}
-            <Route path="/" element={<SignUp />} />
-            <Route path="/login" element={<Login />} />
+           /* These two lines of code are defining public routes in a React application using React
+           Router. */
+            {/* <Route path="/" element={<SignUp />} />
+            <Route path="/login" element={<Login />} /> */}
 
             {/* Protected layout with nested routes */}
-            <Route element={<ProtectedRoute />}>
+            {/* <Route element={<ProtectedRoute />}> */}
+            {/* <Route path="/" element={<Layout />}> */}
               <Route path="/home" element={<Layout />}>
                 <Route index element={<Dashboard />} />
                 <Route path="inventory" element={<InventoryPage />} />
@@ -30,7 +33,7 @@ export default function App() {
                 <Route path="productpurchase" element={<ProductPurchase />} />
                 <Route path="billinvoice" element={<BillInvoice />} />
               </Route>
-            </Route>
+            {/* </Route> */}
 
             {/* Redirect all unknown routes to home */}
             <Route path="*" element={<Navigate to="/" replace />} />
