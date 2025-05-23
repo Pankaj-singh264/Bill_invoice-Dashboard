65712b5df9084c5e853c373b4c389ec2f9da5a5a
Pankaj <pn00532@gmail.com>
2025-05-24 00:14:05 +0530

    added

--- billInvoiceBackEnd/dataBase/db.js
+++ billInvoiceBackEnd/dataBase/db.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose');
 const connectToDB = async () => {
     try {
-        const mongoURI = process.env.MONGODB_URI ;
+        // const mongoURI = process.env.MONGODB_URI ;
         
         // 'mongodb: //username:password@hostname:port/database?ssl=true&replicaSet=myReplicaSet''
+        const mongoURI = 'mongodb+srv://Pankaj:12121212@cluster0.x4yeanv.mongodb.net/'
         await mongoose.connect(mongoURI, {
           
         });
